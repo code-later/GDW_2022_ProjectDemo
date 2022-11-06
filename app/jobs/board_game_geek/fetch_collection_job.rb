62b5ca85a3de97536098f4752035ef5518e351c7
Dirk Breuer <mail@breuer.io>
2022-11-06 11:56:14 +0100

    Update games during import

--- app/jobs/board_game_geek/fetch_collection_job.rb
+++ app/jobs/board_game_geek/fetch_collection_job.rb
@@ -5,15 +5,14 @@ class BoardGameGeek::FetchCollectionJob < ApplicationJob
     bgg_service = BoardGameGeek::Service.new
 
     GameLibrary.transaction(requires_new: true) do
-      game_library.games.destroy_all
+      game_library.games = bgg_service.collection(username: game_library.bgg_username).map do |bgg_game|
+        game = Game.find_or_initialize_by(external_id: bgg_game.id)
+        game.name = bgg_game.name
+        game.picture_url = bgg_game.picture
+        game.rating = bgg_game.rating
+        game.save
 
-      bgg_service.collection(username: game_library.bgg_username).each do |bgg_game|
-        game_library.games.create(
-          name: bgg_game.name,
-          external_id: bgg_game.id,
-          picture_url: bgg_game.picture,
-          rating: bgg_game.rating
-        )
+        game
       end
     end
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,5 @@
 class Game < ApplicationRecord
   has_many :game_libraries
-  has_many :owners, class_name: 'Player', foreign_key: 'player_id'
 
   validates :name, presence: true
   validates :external_id, presence: true
